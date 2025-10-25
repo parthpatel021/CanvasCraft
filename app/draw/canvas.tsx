e80499b594f75ef7b2e66349f8a46844f570daa5
Parth Patel <parthkumar111patel@gmail.com>
2025-10-25 15:49:46 +0530

    Basic start

--- app/draw/canvas.tsx
+++ app/draw/canvas.tsx
@@ -4,15 +4,26 @@ import { Metadata } from "next";
 import ToolBar from "./ui/toolbar/toolbar";
 import useTools from "./hooks/useTools";
 import useCanvas from "./hooks/useCanvas";
+import { useEffect, useState } from "react";
 
 export default function Canvas() {
     const tools = useTools();
-
     const {
         mouseDown,
         mouseMove,
         mouseUp
     } = useCanvas();
+
+    const [size, setSize] = useState({ width: 0, height: 0 });
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({ width: window.innerWidth, height: window.innerHeight });
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
 
     return (
         <div className="h-screen w-full">
@@ -21,8 +32,8 @@ export default function Canvas() {
                 onMouseMove={mouseMove}
                 onMouseDown={(ev) => mouseDown(ev, tools.selectedTool)}
                 onMouseUp={mouseUp}
-                width={window.innerWidth}
-                height={window.innerHeight}
+                width={size.width}
+                height={size.height}
                 id='canvas'
             >Drawing Canvas</canvas>
         </div>
--- app/draw/hooks/useCanvas.ts
+++ app/draw/hooks/useCanvas.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import rough from "roughjs";
 
 export type CanvasHook = {
@@ -12,30 +12,70 @@ export type CanvasHook = {
 
 export default function useCanvas() {
     const [roughCanvas, setRoughCanvas] = useState<ReturnType<typeof rough['canvas']> | null>(null);
+    const [elements, setElements] = useState<any[]>([]);
 
-    const initCanvas = () => {
+    const draw = () => {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
+        if (!canvas || !roughCanvas) {
+            return;
+        }
+        const ctx = canvas?.getContext("2d") as CanvasRenderingContext2D;
+        const generator = roughCanvas.generator;
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const opts =  { stroke: 'white', strokeWidth: 2, bowing: 2, roughness: 2.8, fill: 'blue' };
+        const elementsWithShape = elements.map(ele => {
+            if (!ele.shape) {
+                ele.shape = generator.rectangle(ele.x, ele.y, 200, 200, opts);
+            }
+            return ele;
+        });
+        setElements(elementsWithShape);
+
+        elementsWithShape.forEach((ele) => {
+            roughCanvas.draw(ele.shape);
+        });
+        
+    }
+
+    // Effects
+    useEffect(() => {
+        // Initalize canvas and rendering context
         const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
         if (!canvas) return;
         const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
         const rc = rough.canvas(canvas);
         setRoughCanvas(rc);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-    };
+    }, []);
 
     useEffect(() => {
         if (!roughCanvas) {
-            initCanvas();
             return;
         }
+        draw();
+    }, [roughCanvas, elements.length]);
 
-        roughCanvas.rectangle(100, 100, 200, 200, {stroke: 'white', strokeWidth: 2, bowing: 2, roughness: 2.8, fill: 'blue' });
-        roughCanvas.rectangle(350, 100, 200, 200, {stroke: 'white', strokeWidth: 2, fill: 'green', fillStyle: 'solid'  });
-    }, [roughCanvas]);
+    // Element Handlers
+    const addElement = (elementType: string, x: number, y: number) => {
+        setElements((prev: any) => [...prev, { type: elementType, x, y }]);
+    }
 
-    const mouseDown = (ev: React.MouseEvent<HTMLCanvasElement>, selectedTool:string) => {
+    // Mouse Event Handlers
+    const getViewCoords = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        // Calculate mouse coordinates after applying viewport transformations (pan/zoom)
+        const clientX = event.clientX;
+        const clientY = event.clientY;
+        return { clientX, clientY }
+    }
+
+    const mouseDown = (ev: React.MouseEvent<HTMLCanvasElement>, selectedTool: string) => {
+        const { clientX, clientY } = getViewCoords(ev);
+        addElement(selectedTool, clientX, clientY);
     };
-    const mouseMove = () => console.log("mouseMove");
-    const mouseUp = () => console.log("mouseUp");
+    const mouseMove = () => {};
+    const mouseUp = () => {};
 
     return {
         mouseDown,
